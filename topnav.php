e554c3dbb729e5911c2340bc375222edd4104ae5
Tyler Monaghan <ty.monaghan@gmail.com>
2018-11-06 15:42:39 -0600

    add sotu-db-front content

---
+++ topnav.php
@@ -0,0 +1,13 @@
+<?php
+echo '<div class="w3-bar w3-purple" style="padding-left:10px">
+  <a href="#" class="w3-bar-item w3-button"><b>SOTU-db</b></a>
+  <a href="credits.html" class="w3-bar-item w3-right w3-button w3-hide-small">Credits</a>
+  <a href="documentation.html" class="w3-bar-item w3-right w3-button w3-hide-small w3-border-right">Documentation</a>
+  <a href="https://github.com/tymonaghan/sotu-db" class="w3-bar-item w3-right w3-button w3-hide-small w3-border-right">GitHub Repository <i class="fa fa-external-link"></i></a>
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="showDropMenu()">&#9776;</a>
+</div>
+<div id="mobilemenu" class="w3-bar-block w3-purple w3-hide w3-hide-large w3-hide-medium">
+  <a href="https://github.com/tymonaghan/sotu-db" class="w3-bar-item w3-button w3-right">GitHub Repository <i class="fa fa-external-link"></i></a>
+  <a href="documentation.html" class="w3-bar-item w3-button w3-right">Documentation</a>
+  <a href="credits.html" class="w3-bar-item w3-button w3-right">Credits</a>';
+ ?>
