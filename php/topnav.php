842109efc96957e3a83b851f70312406581da9c0
Ty <ty.monaghan@gmail.com>
2018-12-04 02:07:46 -0600

    fix documentation and credits links

--- php/topnav.php
+++ php/topnav.php
@@ -1,13 +1,13 @@
 <?php
 echo '<div class="w3-bar w3-purple w3-cell-row">
   <a href="/" class="w3-container w3-cell-middle w3-left w3-button w3-xxlarge"><b>SOTU-db</b></a>
-  <a href="html/credits.html" class="w3-container w3-button w3-cell-middle w3-right w3-hide-small" style="padding:20px">Credits</a>
-  <a href="html/documentation.html" class="w3-container w3-button w3-cell-middle w3-hide-small w3-border-right w3-right" style="padding:20px">Documentation</a>
+  <a href="../html/credits.html" class="w3-container w3-button w3-cell-middle w3-right w3-hide-small" style="padding:20px">Credits</a>
+  <a href="../html/documentation.html" class="w3-container w3-button w3-cell-middle w3-hide-small w3-border-right w3-right" style="padding:20px">Documentation</a>
   <a href="https://github.com/tymonaghan/sotu-db" class="w3-right w3-container w3-button w3-cell-middle w3-hide-small w3-border-right" style="padding:20px">GitHub Repo  <i class="fa fa-external-link"></i></a>
   <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="showDropMenu()">&#9776;</a>
   </div>
 <div id="mobilemenu" class="w3-bar-block w3-purple w3-hide w3-hide-large w3-hide-medium">
   <a href="https://github.com/tymonaghan/sotu-db" class="w3-bar-item w3-button w3-right">GitHub Repo <i class="fa fa-external-link"></i></a>
-  <a href="html/documentation.html" class="w3-bar-item w3-button w3-right">Documentation</a>
-  <a href="html/credits.html" class="w3-bar-item w3-button w3-right">Credits</a>
+  <a href="../html/documentation.html" class="w3-bar-item w3-button w3-right">Documentation</a>
+  <a href="../html/credits.html" class="w3-bar-item w3-button w3-right">Credits</a>
   </div>';
