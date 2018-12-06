fe05f143a77c108950540a1d718476779b3c8843
Ty <ty.monaghan@gmail.com>
2018-12-06 00:51:35 -0600

    fix up functionality and design of index, topnav, and add help

--- documentation/toolchain.md
+++ documentation/toolchain.md
@@ -23,5 +23,7 @@
 13. **[dobarkod/cookie-banner](https://github.com/dobarkod/cookie-banner)** JavaScript based cookie-info banner for complying with EU cookie law
 14. **[UMLet](https://www.umlet.com/)** UML diagramming
 15. **[drawio](https://draw.io/)** Software system diagramming
+16. **[Paint.net](https://www.getpaint.net/)** image processing and conversion
+17. **[PDF to Image](https://pdftoimage.com/)** turn PDFs into Images (US Constitution photo)
 
 Where should credit for digital projects begin and end? I relied upon countless software and hardware developers, manufacturers, bloggers, assembly line employees, etc. for the hardware and software on which SOTU-db runs. SOTU-db wouldn't work without employees of the telecoms that serve my house, Loyola University Chicago, and the datacenters where my data lives and whence it is served. The personal and professional networks of which I'm a part, and even Donald Trump, deserve some share of credit for inspiring this project and helping me to envision and instantiate it. If physical exercise is as generative to the creative process as it seems, then even the manufacturers of my ice skates or shoes are part of the toolchain that made SOTU-db possible. Deciding where to draw the line and what products and influences to cite is a subjective and messy process.
--- sotu-js.js
+++ sotu-js.js
@@ -1,13 +1,13 @@
-function showDropMenu() {
+function showDropMenu() { //make topNav responsive to small screens
   var x = document.getElementById("mobilemenu");
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
   } else {
     x.className = x.className.replace(" w3-show", "");
   }
-} //end myFunction
+} //end showDropMenu()
 
-function updateFilterSearch() {
+function updateFilterSearch() { //NOT CURRENTLY USED as of a.2.0, used to update filter search box
   var input, filter, ul, li, a, i;
   input = document.getElementById("filterSearchBox");
   filter = input.value.toUpperCase();
@@ -21,9 +21,9 @@ function updateFilterSearch() {
       li[i].style.display = "none";
     }
   }
-}
+} //end updateFilterSearch()
 
-function loadSOTUforViewer(year) {
+function loadSOTUforViewer(year) { // load content for SOTU viewer in searchbox
   if (year == 1790) {
     document.getElementById("fileViewerFrame").src = "speeches-ucsb-pres-project/1790-01-08-washington.md"
   } else if (year == 1797) {
@@ -31,21 +31,19 @@ function loadSOTUforViewer(year) {
   } else if (year == 2012) {
     document.getElementById("fileViewerFrame").src = "speeches-ucsb-pres-project/2012-01-24-obama.md"
   }
-} //end loadSOTUforViewer
+} //end loadSOTUforViewer()
 
-function loadExtra(extraName){
-//document.getElementsByClassName("extras").style.display="none";
-//document.getElementById(extraName).style.display="block";
-if (extraName=="is-thy-union-here"){
-  document.getElementById("extrasViewerFrame").src="contributors/taylorcate/_Is Thy Union Here__.pdf";
-} else if (extraName=="redacted-unions"){
-  document.getElementById("extrasViewerFrame").src="contributors/RJP43/contribution_01/README.md";
-} else if (extraName == "trivia-of-the-union"){
-  document.getElementById("extrasViewerFrame").src="output/msg/trivia-otu-wait.md";
-}
-}
+function loadExtra(extraName) { //load content in "extras" tab of search box
+  if (extraName == "is-thy-union-here") {
+    document.getElementById("extrasViewerFrame").src = "contributors/taylorcate/_Is Thy Union Here__.pdf";
+  } else if (extraName == "redacted-unions") {
+    document.getElementById("extrasViewerFrame").src = "contributors/RJP43/contribution_01/README.md";
+  } else if (extraName == "trivia-of-the-union") {
+    document.getElementById("extrasViewerFrame").src = "output/msg/trivia-otu-wait.md";
+  }
+} //end extraName()
 
-function openTab(tabName) { //really not sure why i can't get this to work with the w3-hide/w3-show arrangement like showDropMenu, but style=display works for now.
+function openTab(tabName) { //tab switcher in searchBox.html - really not sure why i can't get this to work with the w3-hide/w3-show arrangement like showDropMenu, but style=display works for now.
   var i;
   var tablinks = document.getElementsByClassName("tablink");
   var x = document.getElementsByClassName("tab");
@@ -58,9 +56,9 @@ function openTab(tabName) { //really not sure why i can't get this to work with 
   }
   document.getElementById(tabName).style.display = "block";
   document.getElementById(tabName.concat("", "button")).className += " w3-light-blue";
-} //end openTab function
+} //end openTab()
 
-function includeHTML() {
+function includeHTML() { //w3 css html include
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
   z = document.getElementsByTagName("*");
@@ -90,12 +88,12 @@ function includeHTML() {
       return;
     }
   }
-}
+} //end includeHTML()
 
-function closeModal() {
+function closeModal() { //close restrictions modal when X is pressed
   document.getElementById(" restrictionsModal").style.display = "none";
-}
+} //end closeModal()
 
-function toggleRestrictionsBanner(){
+function toggleRestrictionsBanner() { //close restrictions banner when X is pressed
   document.getElementById(" restrictionsBanner").style.display = "none";
-}
+} //end toggleRestrictionsBanner()
